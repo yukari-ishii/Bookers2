c5e78a797421498aa967637e3013d5a42d96acb8
yukari-ishii <115003222+yukari-ishii@users.noreply.github.com>
2022-11-09 14:01:13 +0000

    [START]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def index
+    #一覧表示のための変数
+    @books = Book.all
+    #新規投稿のための変数
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path #userのshow画面にリダイレクト
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: 'homes#top'
+  
+  resources :books, only: [:index, :create, :show, :edit]
+  get "homes/about" => "homes#about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
